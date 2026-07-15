0d7904cbd13dc78af8f50ea453b63daa6a8767e1
AccelAnalysis <jholman@accelanalysis.com>
2026-07-15 18:34:31 -0400

    Use corrected stabilization selectors directly

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import AuthenticationGate from './components/auth/AuthenticationGate.jsx';
 import MLBDashboard from './app/MLBDashboard.jsx';
 import './index.css';
 import './phase-1-stabilization.css';
-import './modal-contrast.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
