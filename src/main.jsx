3190771e7d9668b7458dfdf4bdb1c04af0af0b81
AccelAnalysis <jholman@accelanalysis.com>
2026-07-14 22:31:24 -0400

    Apply MLB dashboard brand style system

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MLBDashboard from './MLBDashboard_field_complete.jsx';
 import './index.css';
+import './mlb-brand.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
