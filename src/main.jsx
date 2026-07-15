e93dc9c13efdedd5675b64f8a8271e8a60e17999
AccelAnalysis <jholman@accelanalysis.com>
2026-07-14 23:20:27 -0400

    Apply Phase 1 stabilization styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import './mlb-brand.css';
 import './wallboard-display.css';
 import './mlb-logo.css';
+import './phase-1-stabilization.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
